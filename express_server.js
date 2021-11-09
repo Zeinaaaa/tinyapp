bfe6f7de2f2f911756b19774bb7c96ac4940e48c
Zeinaaaa <zainabalbasuny8181@gmail.com>
2021-11-08 19:45:32 -0500

    using the body-parser library to make receive post actions

--- express_server.js
+++ express_server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+function generateRandomString() {
 
+};
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,11 +29,15 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
    let shortURL = req.params.shortURL;
   const templateVars = {shortURL: shortURL, longURL: urlDatabase.shortURL};
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
